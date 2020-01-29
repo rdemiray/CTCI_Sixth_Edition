e7b7c1ede0b2fecf40e8915f936d69cae7a209cc
Muharrem Demiray <muharrem.demiray@gmail.com>
2020-01-29 06:24:40 -0500

    Chapter 1, Question2 has been solved in another way...

--- Chapter_1/Q2.py
+++ Chapter_1/Q2.py
@@ -25,26 +25,47 @@ def check_permutation_2(string1, string2):
     """
     Do not use Counter, use dictionary instead
     """
-    pass
+    # First, check whether string lengths are equal (EARLY EXIT)
+    if len(string1) != len(string2):
+        return False
+    # Key: [string], Value: [Int] E.g. {"a":5, "b": 10, "c":4}
+    char_dictionary = {}
 
+    for item in string1:
+        # Increment if key already exists, add to dictionary first and then increment otherwise
 
+        if item not in char_dictionary.keys():
+            char_dictionary[item] = 1
+        else:
+            char_dictionary[item] += 1
 
+    # Now we've collected all chars in the dictionary
 
+    # Iterate over second string and decrement values each time
+    for item in string2:
 
+        if item in char_dictionary.keys():
+            if char_dictionary[item] == 0:
+                return False
 
+            char_dictionary[item] -= 1
+        else:
+            # A character in the second string is not even in the dictionary
+            return False
 
+    return True
 
 
 class Test(unittest.TestCase):
     test_data_list_true = [
         ("abcd", "dcba"),
+        ("aaabccdddd", "dcbadddcaa"),
     ]
 
     test_data_list_false = [
         ("Muharrem", "Asya"),
+        ("abcd", "dcbaa"),
     ]
-
-
 
     def test_method(self):
 
@@ -54,8 +75,12 @@ class Test(unittest.TestCase):
         for item in self.test_data_list_false:
             self.assertFalse(check_permutation_1(item[0], item[1]))
 
+    def test_method_2(self):
+        for item in self.test_data_list_true:
+            self.assertTrue(check_permutation_2(item[0], item[1]))
 
-
+        for item in self.test_data_list_false:
+            self.assertFalse(check_permutation_2(item[0], item[1]))
 
 
 if __name__ == "__main__":
---
+++ Chapter_1/Q3.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+
+
+def urlify(url_string, length):
+    """
+
+    :param url_string:
+    :param length:
+    :return:
+    """
+    return True
+
+
+
+class TestClass(unittest.TestCase):
+    test_data_list_true = [
+    ]
+
+    test_data_list_false = [
+    ]
+
+
+    def test_method(self):
+        pass
+
+
