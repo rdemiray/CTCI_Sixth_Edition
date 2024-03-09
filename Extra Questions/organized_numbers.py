baa25aab20fde0c013b565f95aa03f0f0f07968e
Muharrem Demiray <muharrem.demiray@gmail.com>
2024-03-08 22:49:46 -0500

    Added unit tests for organized_numbers.py question

--- Extra Questions/organized_numbers.py
+++ Extra Questions/organized_numbers.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def is_organized_number(number):
     """
     Checks if the given number is organized number
@@ -16,9 +19,11 @@ def is_organized_number(number):
 
     if sum == number:
         # Organized number
+        # print(f"{number} is a organized number")
         return True
     else:
         # Not organized number
+        # print(f"{number} is NOT an organized number")
         return False
 
 
@@ -28,6 +33,10 @@ def factorial(number):
     :param number:
     :return:
     """
+    if number < 0:
+        print("Please enter a positive integer or ZERO")
+        return False
+
     if number == 0:
         return 1
     else:
@@ -47,14 +56,64 @@ def find_all_organized_numbers(number):
         if is_organized_number(i):
             list_of_all_organized_numbers.append(i)
 
-    # Print all organized numbers to the console
+    # Print all organized numbers to the console and then return the list to get asserted
 
     for item in list_of_all_organized_numbers:
         print("{} is an organized number \n".format(item))
 
+    return list_of_all_organized_numbers
 
 
-find_all_organized_numbers(40586)
+# find_all_organized_numbers(40586)
 
 
+class TestClass(unittest.TestCase):
+    test_data = [
+        (1, True),
+        (2, True),
+        (145, True),
+        (40585, True),
+        (100, False),
+        (10, False)
+    ]
 
+    test_data_2 = [
+        (40586, [1, 2, 145, 40585])
+    ]
+
+    test_data_3 = [
+        (3, 6),
+        (2, 2),
+        (1, 1),
+        (0, 1),
+        (5, 120),
+        (4, 24),
+        (-1, False)
+    ]
+
+    def test_method(self):
+        """
+
+        :return:
+        """
+
+        # Unit tests for the method "is_organized_number()"
+        for item in self.test_data:
+            self.assertEqual(
+                is_organized_number(item[0]),
+                item[1],
+                msg=f"{item[0]} is an organized number? {item[1]}")
+
+        # Unit test for the method "find_all_organized_numbers()"
+        for item in self.test_data_2:
+            self.assertEqual(
+                find_all_organized_numbers(item[0]),
+                item[1],
+                msg="Assertion failed")
+
+        # Unit tests for the method "factorial()"
+        for item in self.test_data_3:
+            self.assertEqual(
+                factorial(item[0]),
+                item[1],
+                msg=f"{item[0]}! is not equal to {item[1]}!")
